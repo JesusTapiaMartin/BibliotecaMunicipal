a6d754366a7d3b225c87496307f83d0bf9d058ba
BenjaFA <b.fernandez07@ufromail.cl>
2023-11-26 00:28:53 -0300

    create jdialog class

--- src/main/java/Vista/fmrMenu.java
+++ src/main/java/Vista/fmrMenu.java
@@ -1,4 +1,17 @@
 package Vista;
 
-public class fmrMenu {
+import javax.swing.*;
+
+public class fmrMenu extends JFrame {
+    private JPanel  Menu;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+    public fmrMenu(){
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setTitle("Menú");
+        setContentPane(Menu);
+    }
 }
