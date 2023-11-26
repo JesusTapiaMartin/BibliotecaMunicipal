d1c568b8e095d8199b7db108e45df133c13945dc
YoandriVO <y.villarroel01@ufromail.cl>
2023-11-26 01:58:43 -0300

    commit cambios/modificaciones

--- src/main/java/Vista/fmrBuscar.java
+++ src/main/java/Vista/fmrBuscar.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class fmrBuscar extends JDialog {
     private JPanel jpBuscar;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 
     public fmrBuscar() {
         setVisible(true);
