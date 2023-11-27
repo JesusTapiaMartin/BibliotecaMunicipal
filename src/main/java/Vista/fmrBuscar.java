4990d2242e2231a0c3372c0e13622db6b8291ee1
BenjaFA <b.fernandez07@ufromail.cl>
2023-11-27 00:35:06 -0300

    gui buscar

--- src/main/java/Vista/fmrBuscar.java
+++ src/main/java/Vista/fmrBuscar.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class fmrBuscar extends JDialog {
     private JPanel jpBuscar;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton btnPrestar;
+    private JButton btnSalir;
+    private JButton btnBuscar;
+    private JTable tblLibros;
+    private JTextField txtBuscar;
+    private JButton btnDevolver;
 
     public fmrBuscar() {
         setVisible(true);
